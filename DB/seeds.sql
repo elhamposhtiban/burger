bb2a2e8c5f034c78c84bff3de60f73ce8aa11a98
elham poshtiban@gmail.com <55120923+elhamposhtiban@users.noreply.github.com>
2020-01-10 18:05:04 -0600

    creating data base burgerdb and inserting values

---
+++ DB/seeds.sql
@@ -0,0 +1,5 @@
+insert into burgers (burger_name, devoured) values ('animal burger', true);
+insert into burgers (burger_name, devoured) values ('cheese burger', true);
+insert into burgers (burger_name, devoured) values ('double burger', true);
+
+select *from burgers;
