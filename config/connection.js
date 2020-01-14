56bc2109a71c0028d97affaf6a1f16d22d64706d
elham poshtiban@gmail.com <55120923+elhamposhtiban@users.noreply.github.com>
2020-01-14 16:17:50 -0600

    adding jawsdb to connection and write if statement for chosing connection

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,21 @@
 
 
 const mysql = require("mysql");
-const connection = mysql.createConnection({
+let connection; 
+
+
+if (process.env.JAWSDB_URL) {
+ connection = mysql.createConnection(process.env.JAWSDB_URL)
+}
+  else {connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: "root",
     password: "root",
     database: "burgers_db"
-  });
+   })
+}
+
 
   connection.connect(function(err) {
     if (err) {
@@ -19,3 +27,13 @@ const connection = mysql.createConnection({
   
 
   module.exports = connection;
+
+
+
+  // mysql.createConnection({
+  //   host: "localhost",
+  //   port: 3306,
+  //   user: "root",
+  //   password: "root",
+  //   database: "burgers_db"
+  // });
